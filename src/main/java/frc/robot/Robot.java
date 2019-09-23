ed07cba501a3d879776d469795e91c414da1f140
boinary <travbuttons34@gmail.com>
2019-09-23 16:32:10 -0400

    Added Robot.java, RobotMap.java
    init variables in RobotMap.jave
    Robot.java makes it work!

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.ManualDrive;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -50,7 +51,9 @@ public class Robot extends TimedRobot {
    * LiveWindow and SmartDashboard integrated updating.
    */
   @Override
-  public void robotPeriodic() {
+  public void robotPeriodic() 
+  {
+    Manual();
   }
 
   /**
@@ -120,6 +123,7 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     Scheduler.getInstance().run();
+    DriveTrain.Manual(RobotMap.starboardMotor, RobotMap.portMotor, RobotMap.driverStick);
   }
 
   /**
