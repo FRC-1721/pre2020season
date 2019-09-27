36960639832efbdeefa0cb42a07c6d7c09fe1122
Joe S <31870999+KenwoodFox@users.noreply.github.com>
2019-09-27 10:52:18 -0400

    Create Telemetry.java

---
+++ src/main/java/frc/robot/subsystems/Telemetry.java
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * Telemetry is used for delivering data to the drivestation
+ */
+public class Telemetry extends Subsystem {
+
+  public Telemetry() {
+    // Drivetrain
+    //SmartDashboard.putData("Reset Encoders", new DrivetrainResetEncoder());
+  }
+
+  public void update() {
+    // Drivetrain
+    //SmartDashboard.putNumber("Port Encoder Count", Robot.drivetrain.getDriveEncoderCount());
+    //SmartDashboard.putNumber("Starboard Encoder Count", Robot.drivetrain.getDriveEncoderDistance());
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
