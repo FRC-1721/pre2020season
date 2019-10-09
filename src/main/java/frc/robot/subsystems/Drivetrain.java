972c394c857b4fe4e64da7ee6fd8a4280cc5494f
KhanSimeoni <simeonikh@gmail.com>
2019-10-09 16:09:15 -0400

    SmartDashboard Compatibility Added

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -12,6 +12,7 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.RobotMap;
 
 /**
@@ -23,7 +24,19 @@ public class Drivetrain extends Subsystem {
     Double thro = Driverstick.getRawAxis(RobotMap.driverstickThroAxis); //populates "thro" as the angle of the driverstick
     Double yaw = Driverstick.getRawAxis(RobotMap.driverstickYawAxis); //populates "yow" as the angle of the driverstick again
     Port.set(ControlMode.PercentOutput, thro + yaw);
-    Starboard.set(ControlMode.PercentOutput, (thro * -1) + yaw); 
+    Starboard.set(ControlMode.PercentOutput, (thro * -1) + yaw);
+    if(thro >= 1){
+      SmartDashboard.putString("Moving?", "VROOM!" );
+    } else if (thro <= -1){
+      SmartDashboard.putString("Moving?", "BEEP BEEP!");
+    } else {
+      SmartDashboard.putString("Moving?", "Nope");
+    }
+    if(yaw != 0){
+      SmartDashboard.putString("Spin?", "SPIN SPIN!");
+    } else {
+      SmartDashboard.putString("Spin?", "What?");
+    }
   }
   
 
